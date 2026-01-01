5967b4df3d2c90dadc3652d78c7c7a711ce76d6e
Hasanqp <hasangubran6@gmail.com>
2026-01-02 02:42:41 +0300

    feat(pr-service): integrate application layer using MediatR v12
    - Add MediatR 12.2.0 with built-in DI support
    - Centralize MediatR registration in Application layer
    - Expose AddApplication() extension for API integration
    - Keep API decoupled from application use cases

--- Services/PRService.Application/DependencyInjection.cs
+++ Services/PRService.Application/DependencyInjection.cs
@@ -1,15 +1,17 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace PRService.Application
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddApplication(
-            this IServiceCollection services)
+        public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             // MediatR / Validators
+            services.AddMediatR(cfg =>
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
             return services;
         }
-
     }
 }
