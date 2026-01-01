e1df93cf795b33286cf5835cb638aa02ca1afe66
Hasanqp <hasangubran6@gmail.com>
2026-01-02 00:29:19 +0300

    feat(pr-service): implement create purchase request use case

---
+++ Services/PRService.Application/PurchaseRequests/Commands/CreatePurchaseRequest/CreatePurchaseRequestHandler.cs
@@ -0,0 +1,28 @@
+﻿using PRService.Application.Abstractions.Persistence;
+using PRService.Domain.Entities;
+
+namespace PRService.Application.PurchaseRequests.Commands.CreatePurchaseRequest
+{
+    public sealed class CreatePurchaseRequestHandler
+    {
+        private readonly IPurchaseRequestRepository _repository;
+
+        public CreatePurchaseRequestHandler(IPurchaseRequestRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Guid> Handle(CreatePurchaseRequestCommand command, CancellationToken cancellationToken)
+        {
+            var purchaseRequest = new PurchaseRequest(
+                command.RequestNumber,
+                command.Description,
+                command.TotalAmount
+            );
+
+            await _repository.AddAsync(purchaseRequest, cancellationToken);
+
+            return purchaseRequest.Id;
+        }
+    }
+}
