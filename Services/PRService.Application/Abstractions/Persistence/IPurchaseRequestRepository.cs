d852af356aa096bc6adafe509e0bb9643260fb7e
Hasanqp <hasangubran6@gmail.com>
2026-01-02 00:14:21 +0300

    feat(pr-service): add purchase request repository abstraction

---
+++ Services/PRService.Application/Abstractions/Persistence/IPurchaseRequestRepository.cs
@@ -0,0 +1,10 @@
+﻿using PRService.Domain.Entities;
+
+namespace PRService.Application.Abstractions.Persistence
+{
+    public interface IPurchaseRequestRepository
+    {
+        Task AddAsync(PurchaseRequest purchaseRequest, CancellationToken cancellationToken);
+        Task<PurchaseRequest?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    }
+}
