b0e99927fcdf082e8300fff358c4e97896a0209b
Hasanqp <hasangubran6@gmail.com>
2026-01-02 00:36:21 +0300

    chore(pr-service): add application layer dependency injection setup & package
    - add Microsoft.Extensions.DependencyInjection (8.0.1) nuget package

---
+++ Services/PRService.Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace PRService.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(
+            this IServiceCollection services)
+        {
+            // MediatR / Validators
+            return services;
+        }
+
+    }
+}
