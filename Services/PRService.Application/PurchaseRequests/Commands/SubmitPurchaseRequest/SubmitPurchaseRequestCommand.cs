90e1068dd010e9a02a4733ce9e6dfd359e117585
Hasanqp <hasangubran6@gmail.com>
2026-01-03 01:27:38 +0300

    feat(pr-service): add submit purchase request command
    - Create SubmitPurchaseRequestCommand
    - Define intent to submit PR by id
    - Follow CQRS command naming conventions

---
+++ Services/PRService.Application/PurchaseRequests/Commands/SubmitPurchaseRequest/SubmitPurchaseRequestCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace PRService.Application.PurchaseRequests.Commands.SubmitPurchaseRequest
+{
+    public sealed record SubmitPurchaseRequestCommand(
+        Guid PurchaseRequestId
+    ) : IRequest;
+}
