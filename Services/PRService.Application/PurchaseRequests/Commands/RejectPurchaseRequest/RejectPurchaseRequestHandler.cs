6b06a7ca4e3b6d6c9f59b267158cdde5d2b27d7b
Hasanqp <hasangubran6@gmail.com>
2026-01-03 02:12:38 +0300

    feat(pr-service): implement reject purchase request handler
    - Load purchase request from repository
    - Execute domain rejection behavior
    - Persist rejected state
    - Rely on domain rules for validation

---
+++ Services/PRService.Application/PurchaseRequests/Commands/RejectPurchaseRequest/RejectPurchaseRequestHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using PRService.Application.Abstractions.Persistence;
+
+namespace PRService.Application.PurchaseRequests.Commands.RejectPurchaseRequest
+{
+    public sealed class RejectPurchaseRequestHandler : IRequestHandler<RejectPurchaseRequestCommand>
+    {
+        private readonly IPurchaseRequestRepository _repository;
+
+        public RejectPurchaseRequestHandler(IPurchaseRequestRepository repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(RejectPurchaseRequestCommand request, CancellationToken cancellationToken)
+        {
+            var pr = await _repository.GetByIdAsync(
+                request.PurchaseRequestId,
+                cancellationToken
+            );
+
+            if ( pr is null)
+            {
+                throw new Domain.Exceptions.InvalidOperationException(
+                    "Reject PR",
+                    "Purchase request not found"
+                );
+            }
+
+            // Domain enforces rejection rules
+            pr.Reject(request.Reason);
+
+            await _repository.UpdateAsync(pr, cancellationToken);
+
+        }
+    }
+}
